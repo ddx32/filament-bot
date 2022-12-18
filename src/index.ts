e2f936d63e73bba9ddbe4f87a80c12c40fbf9be7
Filip Růžička <filip.ruzicka@prusa3d.cz>
2022-12-18 11:39:26 +0100

    Update dependency

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { load } from "https://deno.land/std@0.168.0/dotenv/mod.ts";
 
 import { generateMessage } from "./modules/generateMessage.ts";
