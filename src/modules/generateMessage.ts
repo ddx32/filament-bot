d3931b521e41c3ecee9d2634fc3d6a711b8b526d
Filip Růžička <filip.ruzicka@prusa3d.cz>
2022-12-21 14:37:42 +0100

    Add fun gifs as empty response

--- src/modules/generateMessage.ts
+++ src/modules/generateMessage.ts
@@ -35,13 +35,43 @@ function getInitialBlocks(scheduled: boolean) {
   ];
 }
 
+function getRandomStringFromArray(array: string[]) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function getNoStockMessageBlocks() {
+  const images = [
+    "https://media.giphy.com/media/Az1CJ2MEjmsp2/giphy.gif",
+    "https://media.giphy.com/media/26hkhPJ5hmdD87HYA/giphy.gif",
+    "https://media.giphy.com/media/giXLnhxp60zEEIkq8K/giphy-downsized-large.gif",
+    "https://media.giphy.com/media/g01ZnwAUvutuK8GIQn/giphy.gif",
+    "https://media.giphy.com/media/baPIkfAo0Iv5K/giphy.gif"
+  ];
+
+  return [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text":
+          "Today, life is not good. It seems that the polymers team has a whole bunch of nothing for us today. Try fetching the inventory status closer to the official pick-up time (Tuesday, Thursday 13:30 - 14:30).",
+      },
+    },
+    {
+      "type": "image",
+      "image_url": getRandomStringFromArray(images),
+      "alt_text": "empty stock",
+    },
+  ];
+}
+
 export function generateMessage(
   filaments: FilamentObject[],
   scheduled = false,
 ) {
   if (filaments.length <= 0) {
     return {
-      text: "Today, life is not good.",
+      blocks: getNoStockMessageBlocks(),
     };
   }
 
