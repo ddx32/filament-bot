62eec381945507621ca5267ca9c1394537c82d48
Filip Ruzicka <filip.ruzicka@prusa3d.cz>
2023-11-30 15:02:57 +0100

    Use UTC time for cron planning

--- src/cron.ts
+++ src/cron.ts
@@ -1,7 +1,7 @@
 import { fetchFilaments } from "./modules/fetchFilaments.ts";
 import { postMessageToDefaultChannel } from "./modules/postMessageToDefaultChannel.ts";
 
-const filamentHours = "23 13 * * 1,4";
+const filamentHours = "23 12 * * 1,4";
 const every10Mins = "*/10 * * * *";
 
 Deno.cron("Update default channel", filamentHours, async () => {
