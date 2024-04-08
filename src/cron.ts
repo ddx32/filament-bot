4f3fb21f58c2c7a7f4ed5492724154cf363585e6
Filip Ruzicka <filip.ruzicka@prusa3d.cz>
2024-04-08 16:14:02 +0200

    Use daylight savings time for cron jobs

--- src/cron.ts
+++ src/cron.ts
@@ -1,7 +1,7 @@
 import { fetchFilaments } from "./modules/fetchFilaments.ts";
 import { postMessageToDefaultChannel } from "./modules/postMessageToDefaultChannel.ts";
 
-const filamentHours = "23 12 * * 2,5";
+const filamentHours = "23 11 * * 2,5";
 const every10Mins = "*/10 * * * *";
 
 Deno.cron("Update default channel", filamentHours, async () => {
